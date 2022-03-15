a1aee40a03f9159629829928792a996ed01d2810
VictorLee <lzs_1993@qq.com>
2022-03-15 20:43:06 +0800

    add lifecycle_node in app_ws.

--- app_ws/build.sh
+++ app_ws/build.sh
@@ -8,5 +8,5 @@ UROS_DIR=../host_ws
 UROS_APP=int32_publisher
 UROS_APP_FOLDER=${UROS_DIR}/app_ws/src/${UROS_APP}
 
-colcon build --metas ${UROS_DIR}/colcon.meta src --cmake-args -DBUILD_TESTING=OFF $@
+colcon build --metas ${UROS_DIR}/colcon.meta ./host-app-colcon.meta src --cmake-args -DBUILD_TESTING=OFF $@
 # colcon build --metas ${UROS_DIR}/colcon.meta ${UROS_APP_FOLDER}/app-colcon.meta src --cmake-args -DBUILD_TESTING=OFF $@
--- host_ws/touch.sh
+++ host_ws/touch.sh
@@ -5,5 +5,5 @@ set -o nounset
 set -o pipefail
 
 # ignore broken packages
-touch src/uros/rclc/rclc_examples/COLCON_IGNORE
-touch src/uros/rclc/rclc_lifecycle/COLCON_IGNORE
+# touch src/uros/rclc/rclc_examples/COLCON_IGNORE
+# touch src/uros/rclc/rclc_lifecycle/COLCON_IGNORE
